66ff11d11147857f9239ab501c9919934981873e
David <david.hastie7@gmail.com>
2023-01-31 22:21:55 +1100

    Requesting location permissions, initialising map coordinates

--- src/screens/RecordRunScreen.js
+++ src/screens/RecordRunScreen.js
@@ -2,62 +2,56 @@ import React, {useState, useEffect} from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
+import { Dimensions } from 'react-native';
 
 
 const RecordRunScreen = () => {
   
-  
-  //Not written by me just for reference
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
+  
+  //Controls zoom level of map
+  const { height, width } = Dimensions.get( 'window' );
+  const LATITUDE_DELTA = 0.005;
+  const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+ 
+  
+  //Ensure that location permissions are enabled
+  //When the component is loaded for the first time, request location permission from user.
+  //If not granted, exit. If granted, get user's current location and store this object in state
   useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-
-
+     (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted'){
+          console.log("ERROR");
+          return;
+          }
+        try{
+          let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+          setLocation(location);
+          console.log(location);
+        }catch(e){
+          console.log("ERROR");
+          return;
+        }
+      })();
+    }, []);  
+   
   return (
     <View>
-        <MapView
-          style={styles.map}
-          showsUserLocation={true}
-          followsUserLocation={true}
-          showsMyLocationButton={true}
-          // initialRegion={{
-          //   //latitude: position.coords.latitude,
-          //   //longitude: position.coords.longitude,
-          //   //latitudeDelta: 0.0922,
-          //   //longitudeDelta: 0.033,
-          // }}
-        />
-        
-        
-        
-        
-        
-        
-        
-        
-        
-          
+        {location ?
+          <MapView
+            style={styles.map}
+            showsUserLocation={true}
+            followsUserLocation={true}
+            showsMyLocationButton={false}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+            }}
+          />
+        : null}
     </View>
   );  
 };
