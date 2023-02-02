5d74833f508dce0131e5c8f3679841a142fe49c8
David <david.hastie7@gmail.com>
2023-02-02 16:25:39 +1100

    Moved get location code to custom hook

---
+++ src/hooks/getCurrentLocation.js
@@ -0,0 +1,29 @@
+import * as Location from 'expo-location';
+
+const getCurrentLocation = async () => {
+  let location = null;
+  let errorMsg = '';
+
+  try {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      errorMsg = 'Permission not granted';
+      return { location, errorMsg };
+    }
+  } catch (e) {
+    errorMsg = 'Permission not granted';
+  }
+  
+  try {
+    location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+  } catch (e) {
+    errorMsg = 'Unable to retrieve user location';
+    return { location, errorMsg };
+  }
+
+  return { location, errorMsg };
+};
+
+export default getCurrentLocation;
--- src/screens/RecordRunScreen.js
+++ src/screens/RecordRunScreen.js
@@ -1,38 +1,25 @@
 import React, {useState, useEffect} from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
 import { Dimensions } from 'react-native';
+import getCurrentLocation from '../hooks/getCurrentLocation';
+
 
 const RecordRunScreen = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState('');
   
   useEffect(() => {
-     (async () => {
-        try{
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted'){
-            setErrorMsg('Permission not granted');
-            return;
-            }           
-        }catch(e){
-          setErrorMsg('Permission not granted');
-        }
-        try{
-          let location1 = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
-          setLocation(location1);
-        }catch(e){
-          setErrorMsg('Unable to retrieve user location');
-          return;
-        }
-      })();
-    }, []);
+    (async () => {
+      const { location: locationResult, errorMsg: errorMsgResult } = await getCurrentLocation();
+      setLocation(locationResult);
+      setErrorMsg(errorMsgResult);
+    })();
+  }, []);
 
   const { height, width } = Dimensions.get( 'window' );
   const LATITUDE_DELTA = 0.005; //Controls zoom level of map
   const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
- 
  
   return (
     <View>
