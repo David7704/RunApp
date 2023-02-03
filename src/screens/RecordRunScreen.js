8f9b9b358bdfddf86e45af63c245de4a9c7325c2
David <david.hastie7@gmail.com>
2023-02-03 17:09:57 +1100

    Center map on user location

--- src/screens/RecordRunScreen.js
+++ src/screens/RecordRunScreen.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect} from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps';
 import { Dimensions } from 'react-native';
 import getCurrentLocation from '../hooks/getCurrentLocation';
@@ -7,8 +7,8 @@ import getCurrentLocation from '../hooks/getCurrentLocation';
 
 const RecordRunScreen = () => {
   const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState('');
-  
+  const [errorMsg, setErrorMsg] = useState('Use effect not run');
+
   //Permissions and setting location (Run only once)
   useEffect(() => {
     (async () => {
@@ -22,31 +22,40 @@ const RecordRunScreen = () => {
   const { height, width } = Dimensions.get( 'window' );
   const LATITUDE_DELTA = 0.005; //Controls zoom level of map
   const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+
   
+  const mapView = React.createRef();
+  const CenterMapOnUser = () => {
+    if (!mapView.current) return;
+    const { coords: { latitude, longitude } } = location;
+    mapView.current.animateToRegion({
+      longitude: longitude,
+      latitude: latitude,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    });
+  };
+ 
   return (
     <View>
         {location ?
           <MapView
+            ref={mapView}
             style={styles.map}
             showsUserLocation={true}
             followsUserLocation={true}
             showsMyLocationButton={false}
             
-            initialRegion={{
-              latitude: 37.8025259,
-              longitude: -122.4351431,
-              latitudeDelta: 0.005,
-              longitudeDelta: 0.0026627218934911242,
-            }}
             region={{
               latitude: location.coords.latitude,
               longitude: location.coords.longitude,
               latitudeDelta: LATITUDE_DELTA,
               longitudeDelta: LONGITUDE_DELTA,
             }}
-  
           />
-        : <Text style={styles.errorText}>{errorMsg}</Text>}
+        : <Text style={styles.errorText}>{errorMsg}</Text>
+       }
+        <TouchableOpacity onPress={CenterMapOnUser}><Text>Center on User</Text></TouchableOpacity>      
     </View>
   );  
 };
